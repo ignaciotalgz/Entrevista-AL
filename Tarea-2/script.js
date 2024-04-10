96ccad68d0912f121647e12c907fb3de3a3e4f4d
nachoalgz <54476706+nachoalgz@users.noreply.github.com>
2024-04-10 14:25:39 -0300

    Commit inicial

---
+++ Tarea-2/script.js
@@ -0,0 +1,41 @@
+/*
+	En Javascript desarrollar funciones para manipular el siguiente array
+	1. Filtrar por nroCuenta
+	2. Filtrar por titularCuenta
+	4. Filtrar por tipoCuenta
+	3. Realizar un débito en una cuenta especificada
+*/
+const arrayCuentas = [
+  {
+    nroCuenta: 5486273622,
+    tipoDeCuenta: "Cuenta Corriente",
+    saldoEnPesos: 27771,
+    titularCuenta: "Abigael Natte",
+  },
+  {
+    nroCuenta: 1183971869,
+    tipoDeCuenta: "Caja de Ahorro",
+    saldoEnPesos: 8675,
+    titularCuenta: "Ramon Connell",
+  },
+  {
+    nroCuenta: 9582019689,
+    tipoDeCuenta: "Caja de Ahorro",
+    saldoEnPesos: 27363,
+    titularCuenta: "Jarret Lafuente",
+  },
+  {
+    nroCuenta: 1761924656,
+    tipoDeCuenta: "Cuenta Corriente",
+    saldoEnPesos: 32415,
+    titularCuenta: "Ansel Ardley",
+  },
+  {
+    nroCuenta: 7401971607,
+    tipoDeCuenta: "Cuenta Unica",
+    saldoEnPesos: 18789,
+    titularCuenta: "Jacki Shurmer",
+  },
+];
+
+// Desarrollo
